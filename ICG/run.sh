a576f739e6c60257fbdb7d7e6b0076fb78428314
manas11 <manasgupta1109@gmail.com>
2020-06-06 10:28:45 +0530

    icg test cases

--- ICG/run.sh
+++ ICG/run.sh
@@ -3,7 +3,7 @@
 function run() {
 	flex scanner.l && yacc -d parser.y && gcc y.tab.c lex.yy.c -w
 	local total_testcases="$1"
-	echo "Running: $total_testcases"
+	echo "Running Test cases for ICG phase $total_testcases"
 	local start=1
 	while [ $start -le $total_testcases ]
 	do
@@ -12,17 +12,17 @@ function run() {
 		do
 			echo -ne "_"
 		done
-		echo -ne "Running TestCase $start"
+		echo -ne "Testcase number $start in progress"
 		for i in {1..40}
 		do
 			echo -ne "_"
 		done
 		printf "\n\n"
-		local filename="tests/test"$start".c"
+		local filename="testfiles/test"$start".c"
 		./a.out $filename
 		((start++))
 	done
 }
 
-number_of_files=`ls -l ./tests/ | egrep -c '^-'`
+number_of_files=`ls -l ./testfiles/ | egrep -c '^-'`
 run $number_of_files
---
+++ ICG/testfiles/test1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+	int x=3,y=2;
+	while(x>y){
+		printf("hello world");
+		x--;
+	}
+	// int i,n;
+	// 	do
+	// 	{
+	// 		printf("hi");
+	// 	}while(i<n);
+}
